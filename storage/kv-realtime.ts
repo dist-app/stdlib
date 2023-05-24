7b10cb11bacbad92602a9e4b782fa9b52e5fd267
Daniel Lamando <dan@danopia.net>
2023-05-24 08:09:45 +0200

    Fix import?

--- storage/kv-realtime.ts
+++ storage/kv-realtime.ts
@@ -2,7 +2,7 @@ import * as ows from "https://deno.land/x/stream_observables@v1.3/mod.ts";
 
 import { ArbitraryEntity } from "../../apis/meta.ts";
 import { EntityStorage } from "../storage.ts";
-import { trace, SpanKind, propagation, context, TextMapGetter, ROOT_CONTEXT, SpanContext } from "https://deno.land/x/observability@v0.3.4/api.ts";
+import { trace, SpanKind, propagation, context, TextMapGetter, ROOT_CONTEXT, SpanContext } from "https://deno.land/x/observability@v0.4.0/opentelemetry/api.js";
 
 type KvRealtimeEvent =
 | {
