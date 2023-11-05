60a391cba54a278bdc84cdbcf9e0313b7eaba709
Daniel Lamando <dan@danopia.net>
2023-11-06 00:35:24 +0100

    Plumb out observing IDB from EntityEngine

--- portable/engine.ts
+++ portable/engine.ts
@@ -18,8 +18,7 @@
 //   } & Record<string,unknown>;
 // }
 
-import type { StreamEvent } from "https://raw.githubusercontent.com/danopia/dist-app-meteor/d755b39892f216f6db4ccbfb85cc19d100e8aed8/imports/entities/core.ts";
-import type { ApiKindEntity, EntityStorage } from "./types.d.ts";
+import type { ApiKindEntity, EntityStorage, StreamEvent } from "./types.d.ts";
 // import { EntityStorage, LayeredNamespace, NamespaceSpecWithImpl } from "../storage.ts";
 
 export type MutationOptions<T extends ApiKindEntity> =
@@ -137,26 +136,15 @@ export class EntityEngine {
   observeEntities<T extends ApiKindEntity>(
     apiVersion: T["apiVersion"],
     kind: T["kind"],
-    filterCb?: (entity: T) => boolean,
+    opts?: {
+      signal?: AbortSignal;
+      // filterCb?: (entity: T) => boolean;
+    },
   ): ReadableStream<StreamEvent<T>> {
     const layer = this.selectNamespaceLayer({
       apiVersion: apiVersion,
     });
-    return new ReadableStream({
-      async start(ctlr) {
-        const list = await layer.listEntities<T>(apiVersion, kind);
-        for (const entity of list) {
-          ctlr.enqueue({
-            kind: 'Creation',
-            snapshot: entity,
-          });
-        }
-        ctlr.enqueue({
-          kind: 'InSync',
-        });
-        console.warn(`TODO: EntityEntity must plumb observeEntity`);
-      },
-    });
+    return layer.observeEntities(apiVersion, kind, opts?.signal);
   }
 
   async getEntity<T extends ApiKindEntity>(
--- portable/types.d.ts
+++ portable/types.d.ts
@@ -17,10 +17,31 @@ export interface EntityStorage {
   insertEntity<T extends ApiKindEntity>(entity: T): Promise<void>;
   listAllEntities(): Promise<ApiKindEntity[]>;
   listEntities<T extends ApiKindEntity>(apiVersion: T["apiVersion"], kind: T["kind"]): Promise<T[]>;
-  // TODO: observeEntities()
-  // watchEntities<T extends ApiKindEntity>(apiVersion: T["apiVersion"], kind: T["kind"]);
+  observeEntities<T extends ApiKindEntity>(apiVersion: T["apiVersion"], kind: T["kind"], signal?: AbortSignal): ReadableStream<StreamEvent<T>>;
   getEntity<T extends ApiKindEntity>(apiVersion: T["apiVersion"], kind: T["kind"], name: string): Promise<T | null>;
   updateEntity<T extends ApiKindEntity>(newEntity: T): Promise<void>;
   deleteEntity<T extends ApiKindEntity>(apiVersion: T["apiVersion"], kind: T["kind"], name: string): Promise<boolean>;
   // TODO: add some sort of API for 'submissions' (either entity spec/status or fetch request/response)
 }
+
+export type StreamEvent<T extends ApiKindEntity> =
+  | {
+    kind:
+      | 'InSync'
+      | 'LostSync'
+      | 'Bookmark'
+    ;
+  }
+  | {
+    kind:
+      | 'Creation'
+      | 'Mutation'
+      | 'Deletion'
+    ;
+    snapshot: T;
+  }
+  | {
+    kind: 'Error';
+    message: string;
+  }
+;
